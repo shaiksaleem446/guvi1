fa5cfc94b3e540c629d1aaa66515f4210dee9201
shaiksaleem446 <41343286+shaiksaleem446@users.noreply.github.com>
2018-11-20 06:55:22 -0800

    Create even.c

---
+++ even.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the value of :");
+    scanf("%d",&a);
+    if(a%2==0)
+    
+    {
+        printf("Even");
+    }
+    
+    else
+    {
+        printf("Odd");
+    }
+    
+
+    return 0;
+}
