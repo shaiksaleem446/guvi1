a65953bbe31efb097ef389e81e575ed9b09bb7db
shaiksaleem446 <41343286+shaiksaleem446@users.noreply.github.com>
2018-11-20 07:01:20 -0800

    Update even.c

--- even.c
+++ even.c
@@ -5,7 +5,11 @@ int main()
     int a;
     printf("enter the value of :");
     scanf("%d",&a);
-    if(a%2==0)
+    if (a<0)
+    {
+        printf("Invalid");
+    }
+    else if(a%2==0)
     
     {
         printf("Even");
