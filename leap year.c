0ef3dec2946098324d9136d535ff04af92cf96c1
shaiksaleem446 <41343286+shaiksaleem446@users.noreply.github.com>
2018-11-21 09:16:21 -0800

    Create leap year.c

---
+++ leap year.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter the year:");
+    scanf("%d",&a);
+    if(a%4==0)
+    
+    {
+        printf("yes");
+        
+    }
+    
+    else
+    {
+        printf("no");
+    }
+
+    return 0;
+}
