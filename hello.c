631c51785c1275cf46d7cf818b6ca2dfb26fcc9a
shaiksaleem446 <41343286+shaiksaleem446@users.noreply.github.com>
2018-11-21 09:20:22 -0800

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,i;
+    printf("enter the value:");
+    scanf("%d",&a);
+    for(i=1;i<=a;i++)
+    {
+        printf("Hello \n");
+        
+    }
+    
+    return 0;
+}
