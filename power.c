b71517acbc45ea5571191f02cf903679441f9f40
shaiksaleem446 <41343286+shaiksaleem446@users.noreply.github.com>
2018-11-21 21:02:18 -0800

    Create power.c

---
+++ power.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include<math.h>
+
+int main()
+{
+    int i,n,m;
+    printf("enter the number:");
+    scanf("%d%d",&n,&m);
+    i=pow(n,m);
+    
+
+    printf("%d", i);
+
+
+    return 0;
+}
